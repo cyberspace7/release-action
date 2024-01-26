0e4a16a4ed39c797ab435a9c6376472d66586c43
Benjamin Guibert <contact@bguibert.com>
2024-01-26 23:43:21 +0700

    feat(pr): remove diff comment (#103)

--- src/libs/release-notes.ts
+++ src/libs/release-notes.ts
@@ -1,5 +1,3 @@
-import { diffLines } from "diff";
-
 export function createReleasePullRequestBody(releaseNotes: string) {
   const lines = releaseNotes.split("\n");
   const endIndex = lines.findIndex(
@@ -9,30 +7,4 @@ export function createReleasePullRequestBody(releaseNotes: string) {
   );
   const selectedLines = endIndex > 0 ? lines.splice(2, endIndex - 2) : lines;
   return selectedLines.join("\n");
-}
-
-export function getDiffMarkdown(oldContent: string, newContent: string) {
-  const diff = diffLines(oldContent.trim() + "\n", newContent.trim() + "\n");
-  const markdownDiff = diff
-    .map(({ added, removed, value }) => {
-      const printedValue = value.replace(/\n$/, "");
-      if (removed) {
-        return printedValue
-          .split("\n")
-          .map((line) => `-  ${line}`)
-          .join("\n");
-      }
-      if (added) {
-        return printedValue
-          .split("\n")
-          .map((line) => `+  ${line}`)
-          .join("\n");
-      }
-
-      return null;
-    })
-    .filter((value) => !!value)
-    .join("\n");
-
-  return markdownDiff.length ? `\`\`\`diff\n${markdownDiff}\n\`\`\`` : null;
 }
